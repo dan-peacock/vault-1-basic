cdd4fcbc1eedf5bf5aeafa7f03a54e75bc1d4981
danpeacock <danpeacock96@gmail.com>
2022-09-07 12:59:03 +0100

    init commit

---
+++ vault.tf
@@ -0,0 +1,28 @@
+# Create Vault Cluster
+provider "hcp" {
+  client_id     = var.hcp_client_id
+  client_secret = var.hcp_secret_id
+}
+
+resource "hcp_vault_cluster" "vault_cluster" {
+  hvn_id     = var.hcp_net_id
+  cluster_id = "demo-cluster"
+  public_endpoint = true
+  tier = "starter_small"
+}
+
+resource "hcp_vault_cluster_admin_token" "vault_admin_token" {
+  cluster_id = hcp_vault_cluster.example_vault_cluster.cluster_id
+}
+
+# Configure Secrets Engine
+
+
+# Configure Policy
+
+
+#Outputs
+output "vault_details" {
+    admin_token = hcp_vault_cluster_admin_token.vault_admin_token.token
+    url = hcp_vault_cluster.vault_cluster.vault_public_endpoint_url
+}
