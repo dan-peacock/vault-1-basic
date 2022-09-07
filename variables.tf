a40f4dc81542653155ca7229f10431e3d38cf24f
danpeacock <danpeacock96@gmail.com>
2022-09-07 13:02:58 +0100

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable "hcp_client_id" {
-  uaRWstZCtsDPX7aLbXNQMlrbwGmW2JhY
 }
 
 variable "hcp_secret_id" {
-  ZSr6Rg56ZTNIEztXmDl3PtcALZScoEK5CawjE6c2ZxYIB0gDNX6xhetlVDJf2EMy
 }
 
 variable "hcp_net_id" {
