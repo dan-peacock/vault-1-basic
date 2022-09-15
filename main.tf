23c3e6fc3386f035313101f7cb11e99536f48775
danpeacock <danpeacock96@gmail.com>
2022-09-15 10:59:52 +0100

    add enginge config

--- main.tf
+++ main.tf
@@ -15,7 +15,31 @@ resource "hcp_vault_cluster_admin_token" "vault_admin_token" {
   cluster_id = hcp_vault_cluster.vault_cluster.cluster_id
 }
 
-# Add to Terraform Variable Set
+# Configure Secret Engines
+provider "vault" {
+  address = hcp_vault_cluster.vault_cluster.vault_public_endpoint_url
+  token = hcp_vault_cluster_admin_token.vault_admin_token.token
+}
+
+module "vault_aws_secret_backend" {
+  source = "./modules/aws"
+  count = var.aws_enabled ? 1 : 0
+
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+}
+
+module "vault_azure_secret_backend" {
+  source = "./modules/azure"
+  count = var.azure_enabled ? 1 : 0
+
+  subscription_ID = var.subscription_ID
+  tenant_ID = var.tenant_ID
+  SP_Password = var.SP_Password
+  SP_AppID = var.SP_AppID
+}
+
+# Add to Vault Info to Terraform Variable Set
 provider "tfe" {
   token = var.tfc_token
 }
@@ -47,4 +71,6 @@ resource "tfe_variable" "vault_token" {
   description     = "Vault admin token"
   variable_set_id = tfe_variable_set.vault_details.id
 }
+
+
 
--- variables.tf
+++ variables.tf
@@ -1,17 +1,27 @@
-variable "hcp_client_id" {
-}
+variable "hcp_client_id" {}
 
-variable "hcp_secret_id" {
-}
+variable "hcp_secret_id" {}
 
 variable "hcp_net_id" {
   default = "hvn-vault"
 }
 
-variable "tfc_token" {
-  
-}
+variable "tfc_token" {}
 
-variable "tfc_org_name" {
-  
-}
+variable "tfc_org_name" {}
+
+variable "aws_enabled" {}
+
+variable "azure_enabled" {}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "subscription_ID" {}
+
+variable "tenant_ID" {}
+
+variable "SP_Password" {}
+
+variable "SP_AppID" {}
